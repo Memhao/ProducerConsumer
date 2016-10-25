edf2ce696068fd5735c4ae2b91755b1aa9f3d21c
Xander <xander.shandor@gmail.com>
2016-10-26 00:14:40 +0300

    Add files via upload

--- Consumer.java
+++ Consumer.java
@@ -1,11 +1,40 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.BlockingQueue;
 
 public class Consumer implements Runnable{
 	private final BlockingQueue<Message> messages;
-	public Consumer(BlockingQueue<Message> messages)
+	private String id;
+	private String name;
+	private PrintWriter out;
+	public Consumer(String id,String name, BlockingQueue<Message> messages)
 	{
+		this.id = id;
+		this.name = name ;
 		this.messages = messages;
+		try {
+			String fileName = "src/out"+this.name+".txt";
+			
+			Path path = Paths.get(fileName);
+			System.out.println(path.toString());
+			if(Files.exists(path))
+				out = new PrintWriter(new FileWriter(fileName+"1"));	
+			else 
+				out = new PrintWriter(new FileWriter(fileName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	public String getId()
+	{
+		return this.id;
+	}
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -14,8 +43,13 @@ public class Consumer implements Runnable{
 			try {
 				Message m = messages.take();
 				if(m.getSeverity() == Severity.EOF)
+				{
+					out.close();
 					break;
-				System.out.println(m+"[CONSUMER]");
+				}
+			
+				System.out.println(m+"[CONSUMER-"+this.name+"]");
+				out.write(m.toString()+"\n");
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				Thread.currentThread().interrupt();
--- Main.java
+++ Main.java
@@ -4,20 +4,33 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Main {
 	public static void main(String arg[])
 	{
+		
+		
+
+
 		BlockingQueue<Message> block0 = new LinkedBlockingQueue<Message>();
-		Producer p0 = new Producer(block0, "src/file.txt");
-		Consumer c0 = new Consumer(block0);
+		Producer p0 = new Producer("1",block0, "src/file.txt");
+		Consumer c0 = new Consumer("1","c0",block0);
 		BlockingQueue<Message> block1 = new LinkedBlockingQueue<Message>();
-		Producer p1 = new Producer(block1, "src/flie.txt");
-		Consumer c1 = new Consumer(block1);
+		Producer p1 = new Producer("2",block1, "src/flie.txt");
+		Consumer c1 = new Consumer("2","c1",block1);
 		Thread tp0 = new Thread(p0);
 		Thread tc0 = new Thread(c0);
 		Thread tp1 = new Thread(p1);
 		Thread tc1 = new Thread(c1);
+		
+//		Configuration config = new Configuration();
+//		config.setMaxNoOfClients(2);
+//		config.setNoOfThreadPerClient(1);
+//		Server s = new Server("", config);
+//		s.connect(p1);
+//		s.connect(p0);
+//		s.startServer();
 		tp0.start();
 		tc0.start();
 		tp1.start();
 		tc1.start();
+
 
 	}
 }
